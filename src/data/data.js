6ca26fa7b96ea1a4c5a94cd77c775674d1d754d9
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-16 16:06:25 -0300

    create dataFile

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+const data = {
+  GST3D: [
+    {
+      type: "PLA+",
+      price: 11500,
+    },
+  ],
+
+  Fremover: [
+    {
+      type: "PLA+ | High Speed ",
+      price: 14500,
+    },
+  ],
+
+  Printalot: [
+    {
+      type: "PLA",
+      price: 18000,
+    },
+    {
+      type: "PLA Flex",
+      price: 21700,
+    },
+    {
+      type: "PLA MAX",
+      price: 25100,
+    },
+    {
+      type: "PLA Art",
+      price: 24050,
+    },
+    {
+      type: "PLA Art | Wood",
+      price: 33850,
+    },
+    {
+      type: "PETG",
+      price: 19050,
+    },
+    {
+      type: "ABS",
+      price: 19650,
+    },
+  ],
+
+  Hellbot: [
+    {
+      type: "PLA | Ecofila",
+      price: 12300,
+    },
+  ],
+};
+
+export default data;
